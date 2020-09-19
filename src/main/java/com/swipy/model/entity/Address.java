648593b14b6da15d1cac96155ebeb7c054e8a9a7
nadia alfak <nadia_alfak@icloud.com>
2020-09-19 21:07:30 +0430

    user model done..

--- src/main/java/com/swipy/model/entity/Address.java
+++ src/main/java/com/swipy/model/entity/Address.java
@@ -3,19 +3,30 @@ package com.swipy.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
 @Setter
 @Getter
 public class Address {
-@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "city_id", referencedColumnName = "id", nullable = false)
+    private City city;
+    @Column(nullable = false)
+    private int postalCode;
+    @Column(nullable = false)
+    private int houseNumber;
+    private String description;
+    @ManyToOne
+    @JoinColumn(name = "address_type_id",referencedColumnName = "id",nullable = false)
+    private AddressType addressType;
 
 
 
