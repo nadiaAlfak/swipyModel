6fcc418638f57bc73116c8663f742ece573a4968
nadia alfak <nadia_alfak@icloud.com>
2020-09-19 20:48:55 +0430

    user model done.

--- src/main/java/com/swipy/model/entity/Product.java
+++ src/main/java/com/swipy/model/entity/Product.java
@@ -3,10 +3,8 @@ package com.swipy.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -16,4 +14,39 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    @ManyToOne
+    @JoinColumn(name ="user_id",referencedColumnName = "id",nullable = false)
+    private User user;
+    @OneToMany(targetEntity = Media.class,mappedBy = "product",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    private List<Media> media;
+    @Column(length = 150,nullable = false)
+    private String size;
+    @Column(nullable = false)
+    private double price;
+    private String description;
+    @ManyToOne
+    @JoinColumn(name = "sub_category_id",referencedColumnName ="id",nullable = false )
+    private SubCategory subCategory;
+    private String tag;
+    private int aging;
+    private boolean isDangerous;
+
+
+
+
+
+
+
+    public Product() {}
+    public Product(String size, double price, String description, String tag, int aging, boolean isDangerous) {
+        this.size = size;
+        this.price = price;
+        this.description = description;
+        this.tag = tag;
+        this.aging = aging;
+        this.isDangerous = isDangerous;
+    }
+
+
+
 }
