d0c5dd2217f1ebf77798fa7b4bcf5e472447a3bd
nadia alfak <nadia_alfak@icloud.com>
2020-09-19 21:29:56 +0430

    Country modell add

--- src/main/java/com/swipy/model/entity/Country.java
+++ src/main/java/com/swipy/model/entity/Country.java
@@ -3,10 +3,7 @@ package com.swipy.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
@@ -17,4 +14,15 @@ public class Country {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    @Column(length = 100,nullable = false )
+    private String name;
+
+    public Country() {
+    }
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+
 }
