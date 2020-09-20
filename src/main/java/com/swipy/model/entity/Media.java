cfdb13616e1999b1c029c30a8b2ad71b167a4214
nadia alfak <nadia_alfak@icloud.com>
2020-09-20 11:43:09 +0430

    add media

--- src/main/java/com/swipy/model/entity/Media.java
+++ src/main/java/com/swipy/model/entity/Media.java
@@ -3,10 +3,7 @@ package com.swipy.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
@@ -16,4 +13,27 @@ public class Media {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    @ManyToOne
+    @JoinColumn(name = "product_id",referencedColumnName = "id",nullable = false)
+    private Product product;
+    @Column(length = 10,nullable = false)
+    private String format;
+    private boolean isprimery;
+    @Column(length = 10,nullable = false)
+    private String mediaType;
+
+    public Media(String format, String address, boolean isprimery,String mediaType) {
+        this.format = format;
+        this.address = address;
+        this.isprimery = isprimery;
+        this.mediaType = mediaType;
+    }
+
+    @Column(length = 900,nullable = false)
+    private String address;
+
+    public Media() {
+    }
+
+
 }
