577b64a8e72cb4e574925b0024e7b933175b7908
Sadjad Hoodgar <sadjad.hoodgar@kfzteile24.de>
2020-09-20 17:30:10 +0200

    expenses model added.

--- src/main/java/com/swipy/model/entity/AccExpensis.java
+++ src/main/java/com/swipy/model/entity/AccExpensis.java
@@ -3,10 +3,7 @@ package com.swipy.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
@@ -16,4 +13,21 @@ public class AccExpensis {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    @ManyToOne
+    @JoinColumn(name ="user_id",referencedColumnName = "id",nullable = false)
+    private User user;
+    @Column(nullable = false)
+    private String type;
+    @Column(nullable = false)
+    private double price;
+    private String description;
+
+    public AccExpensis() {
+    }
+
+    public AccExpensis(String type, double price, String description) {
+        this.type = type;
+        this.price = price;
+        this.description = description;
+    }
 }
