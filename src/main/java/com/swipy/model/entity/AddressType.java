e5feafe033bf1d5dafd9fe2e67817e6629ee9c9c
nadia alfak <nadia_alfak@icloud.com>
2020-09-20 11:18:46 +0430

    add AddressType

--- src/main/java/com/swipy/model/entity/AddressType.java
+++ src/main/java/com/swipy/model/entity/AddressType.java
@@ -3,10 +3,7 @@ package com.swipy.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
@@ -16,4 +13,17 @@ public class AddressType {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    @Column(length = 30,nullable = false)
+    private String name;
+
+    public AddressType(String name) {
+        this.name = name;
+    }
+
+
+
+    public AddressType() {
+    }
+
+
 }
