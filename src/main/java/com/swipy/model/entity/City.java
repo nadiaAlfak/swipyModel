b683b46062533fb1bf4b047993662c9c9d5e00f8
nadia alfak <nadia_alfak@icloud.com>
2020-09-20 11:00:46 +0430

    City modell add

--- src/main/java/com/swipy/model/entity/City.java
+++ src/main/java/com/swipy/model/entity/City.java
@@ -3,10 +3,7 @@ package com.swipy.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
@@ -16,4 +13,9 @@ public class City {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    @ManyToOne
+    @JoinColumn(name = "State_id",referencedColumnName = "id",nullable = false)
+    private State state;
+    @Column(length = 100,nullable = false)
+    private String name;
 }
