1745b8cf68eb92fdbaf9ed00192a627d24ad056e
nadia alfak <nadia_alfak@icloud.com>
2020-09-20 19:23:09 +0430

    add sub categury

--- src/main/java/com/swipy/model/entity/SubCategory.java
+++ src/main/java/com/swipy/model/entity/SubCategory.java
@@ -3,10 +3,7 @@ package com.swipy.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
@@ -16,4 +13,16 @@ public class SubCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    @ManyToOne
+    @JoinColumn(name = "category_id",referencedColumnName = "id",nullable = false)
+    private Category category;
+    @Column(length = 100,nullable = false)
+    private String name;
+
+    public SubCategory(String name) {
+        this.name = name;
+    }
+
+    public SubCategory() {
+    }
 }
