df1ea3f1777518c368f2a1a62ce9e0a76734bddb
Sadjad Hoodgar <sadjad.hoodgar@kfzteile24.de>
2020-09-20 17:39:34 +0200

    transaction model added.

--- src/main/java/com/swipy/model/entity/AccTransaction.java
+++ src/main/java/com/swipy/model/entity/AccTransaction.java
@@ -3,10 +3,9 @@ package com.swipy.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.sql.Time;
+import java.util.Date;
 import java.util.UUID;
 
 @Entity
@@ -15,6 +14,31 @@ import java.util.UUID;
 public class AccTransaction {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-private UUID id;
+    private UUID id;
+    @ManyToOne
+    @JoinColumn(name ="user_id",referencedColumnName = "id",nullable = false)
+    private User user;
+    @Column(nullable = false)
+    private Date date;
+    @Column(nullable = false)
+    private Date time;
+    @Column(nullable = false)
+    private double price;
+    private String activity;
+    @Column(length = 150)
+    private String transactionId;
+    @ManyToOne
+    @JoinColumn(name ="order_details_id",referencedColumnName = "id",nullable = false)
+    private OrderDetails orderDetails;
 
+    public AccTransaction() {
+    }
+
+    public AccTransaction(Date date, Date time, double price, String activity, String transactionId) {
+        this.date = date;
+        this.time = time;
+        this.price = price;
+        this.activity = activity;
+        this.transactionId = transactionId;
+    }
 }
