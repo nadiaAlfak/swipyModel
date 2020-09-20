ae2f65ec37dddce483b276a5eee7e86d24125431
Sadjad Hoodgar <sadjad.hoodgar@kfzteile24.de>
2020-09-20 14:17:48 +0200

    repository folder added

--- src/main/java/com/swipy/model/entity/Media.java
+++ src/main/java/com/swipy/model/entity/Media.java
@@ -34,6 +34,4 @@ public class Media {
 
     public Media() {
     }
-
-
 }
---
+++ src/main/java/com/swipy/model/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.swipy.model.repository;
+
+import com.swipy.model.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, UUID> {
+}
