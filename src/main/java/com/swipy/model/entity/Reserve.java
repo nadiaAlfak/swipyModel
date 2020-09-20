1738a600fe186d92ee97f7beabfea77c873eae12
nadia alfak <nadia_alfak@icloud.com>
2020-09-20 14:08:17 +0430

    add reserv

--- src/main/java/com/swipy/model/entity/Reserve.java
+++ src/main/java/com/swipy/model/entity/Reserve.java
@@ -3,10 +3,8 @@ package com.swipy.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 import java.util.UUID;
 
 @Entity
@@ -16,4 +14,31 @@ public class Reserve {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    @Column(nullable = false)
+    private Date startDate;
+    @Column(nullable = false)
+    private Date endDate;
+    @ManyToOne
+    @JoinColumn(name = "product_id",referencedColumnName = "id",nullable = false)
+    private Product product;
+    @ManyToOne
+    @JoinColumn(name = "user_id",referencedColumnName = "id",nullable = false)
+    private User user;
+    @Column(length = 50)
+    private String status;
+    @Column(length = 50)
+    private String peymentStatus;
+
+
+    public Reserve(Date startDate, Date endDate, String status, String peymentStatus) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+        this.peymentStatus = peymentStatus;
+    }
+
+
+    public Reserve() {
+    }
+
 }
