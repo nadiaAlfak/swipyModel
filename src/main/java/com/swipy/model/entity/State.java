3e091f18e7cd2fe33aa264f173858204a34685dd
nadia alfak <nadia_alfak@icloud.com>
2020-09-20 11:24:34 +0430

    add State

--- src/main/java/com/swipy/model/entity/State.java
+++ src/main/java/com/swipy/model/entity/State.java
@@ -3,10 +3,7 @@ package com.swipy.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
@@ -16,4 +13,18 @@ public class State {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    @ManyToOne
+    @JoinColumn(name = "country_id",referencedColumnName = "id",nullable = false)
+    private Country country;
+
+    @Column(length = 100,nullable = false)
+    private String name;
+
+    public State(String name) {
+        this.name = name;
+    }
+
+    public State() {
+    }
+
 }
