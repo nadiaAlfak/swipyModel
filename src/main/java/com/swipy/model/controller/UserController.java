47f8559ab2056a2b53621841e92ca58cc2c708cd
nadia alfak <nadia_alfak@icloud.com>
2020-09-21 21:50:46 +0330

    add new user controller

--- src/main/java/com/swipy/model/controller/UserController.java
+++ src/main/java/com/swipy/model/controller/UserController.java
@@ -1,4 +1,18 @@
 package com.swipy.model.controller;
 
+import com.google.gson.Gson;
+import com.swipy.model.entity.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    @PostMapping("/newUser")
+    public void register(@RequestBody String newUser){
+        Gson gson=new Gson();
+        User user=gson.fromJson(newUser,User.class);
+
+    }
+
 }
