8019bbdd56b54f79cc67fe4350971ad4a087ee34
nadia alfak <nadia_alfak@icloud.com>
2020-10-03 22:02:08 +0330

    add new

---
+++ src/main/java/com/swipy/model/businessLogic/UserCRUD.java
@@ -0,0 +1,9 @@
+package com.swipy.model.businessLogic;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserCRUD {
+    public boolean save()
+
+}
--- src/main/java/com/swipy/model/controller/UserController.java
+++ src/main/java/com/swipy/model/controller/UserController.java
@@ -1,18 +1,40 @@
 package com.swipy.model.controller;
 
 import com.google.gson.Gson;
+import com.swipy.model.businessLogic.UserCRUD;
 import com.swipy.model.entity.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
+    UserCRUD userCRUD;
+    @Autowired
+    public void setUserCRUD(UserCRUD userCRUD) {
+        this.userCRUD = userCRUD;
+    }
+
+
+
     @PostMapping("/newUser")
-    public void register(@RequestBody String newUser){
+    public boolean register(@RequestBody String newUser){
         Gson gson=new Gson();
         User user=gson.fromJson(newUser,User.class);
+        return true;
 
     }
+    @PostMapping("/updateUser")
+    public boolean update(@RequestBody String existUser){
+        Gson gson = new Gson();
+        User exist_User=gson.fromJson(existUser,User.class);
+        return true;
+        //todo:password should never be update here.
+
+    }
+    @GetMapping("/user")
+    public String getUser(@RequestParam String id){
+        return null;
+    }
+
 
 }
